6509dfaa76fcb8614f3ba56dff51db3a84604d1f
Oleg Rachkovan <rachkovan@startext.tomsk.ru>
2017-12-18 19:10:47 +0700

    update controller. add getting map from string

--- src/main/java/tpu/timetracker/backend/rest/GraphQLController.java
+++ src/main/java/tpu/timetracker/backend/rest/GraphQLController.java
@@ -1,5 +1,7 @@
 package tpu.timetracker.backend.rest;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
@@ -7,6 +9,7 @@ import graphql.GraphQLException;
 import graphql.schema.GraphQLSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +21,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import tpu.timetracker.backend.graphql.RuntimeWiringBinder;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @CrossOrigin(origins = "*")
@@ -27,16 +32,38 @@ public class GraphQLController {
 
   private static final Logger log = LoggerFactory.getLogger(GraphQLController.class);
 
+  @Autowired
+  private ObjectMapper jacksonObjectMapper;
+
+  private TypeReference<HashMap<String, Object>> typeRefReadJsonString = new TypeReference<HashMap<String, Object>>() {};
+
+  private Map<String, Object> getVariablesMapFromString(String variablesFromRequest) {
+    try {
+      return jacksonObjectMapper.readValue(variablesFromRequest, typeRefReadJsonString);
+    } catch (IOException exception) {
+      throw new GraphQLException("Cannot parse variables", exception);
+    }
+  }
+
   @SuppressWarnings("unchecked")
   private Map<String, Object> getVariablesFromRequest(Map<String, Object> body) {
-    Object varsFromBody = body.get("vars");
-    if (StringUtils.isEmpty(varsFromBody)){
+    Object variablesFromRequest = body.get("vars");
+
+    if (variablesFromRequest == null) {
       return Collections.emptyMap();
-    } else if (varsFromBody instanceof Map) {
-      return (Map<String, Object>) varsFromBody;
+    }
+
+    if (variablesFromRequest instanceof String) {
+      if (StringUtils.hasText((String) variablesFromRequest)) {
+        return getVariablesMapFromString((String) variablesFromRequest);
+      }
+    } else if (variablesFromRequest instanceof Map) {
+      return (Map<String, Object>) variablesFromRequest;
     } else {
       throw new GraphQLException("Variables must be a Map");
     }
+
+    return Collections.emptyMap();
   }
 
   @RequestMapping(value = "/graphql", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, method = RequestMethod.POST)
